9e6bfafa06b056cebb98b4fb2447aceadd567302
Carol Johnson <carjohnson@gmail.com>
2025-05-28 20:42:30 -0400

    WIP - Empty mode shell -  restarted the ohif-mode-webquiz/src/index.tsx ; It is now a shell that loads properly but there are no toolbar buttons and there  is no functionality for measurements, segmentation

--- ohif-modes/ohif-mode-webquiz/src/index.tsx
+++ ohif-modes/ohif-mode-webquiz/src/index.tsx
@@ -1,113 +1,51 @@
+import toolbarButtons from './toolbarButtons';
 import { hotkeys } from '@ohif/core';
-import { toolbarButtons } from '@ohif/mode-longitudinal';
 import { id } from './id';
+import i18n from 'i18next';
 import initToolGroups from './initToolGroups';
-// import toolbarButtons from './toolbarButtons';
-import segmentationButtons from './segmentationButtons';
-import moreTools from './moreTools';
 
-
+const configs = {
+  Length: {},
+  //
+};
 const ohif = {
   layout: '@ohif/extension-default.layoutTemplateModule.viewerLayout',
   sopClassHandler: '@ohif/extension-default.sopClassHandlerModule.stack',
   hangingProtocol: '@ohif/extension-default.hangingProtocolModule.default',
   leftPanel: '@ohif/extension-default.panelModule.seriesList',
-  rightPanel: '@ohif/extension-cornerstone.panelModule.panelMeasurement',
+  // rightPanel: '@ohif/extension-cornerstone.panelModule.panelMeasurement',
 };
 
 const cornerstone = {
+  segpanel: '@ohif/extension-cornerstone.panelModule.panelSegmentation',
+  measurements: '@ohif/extension-cornerstone.panelModule.panelMeasurement',
   viewport: '@ohif/extension-cornerstone.viewportModule.cornerstone',
 };
 
-const segmentation = {
-  sopClassHandler: '@ohif/extension-cornerstone-dicom-seg.sopClassHandlerModule.dicom-seg',
-  viewport: '@ohif/extension-cornerstone-dicom-seg.viewportModule.dicom-seg',
-  panelTool: '@ohif/extension-cornerstone.panelModule.panelSegmentationWithTools',
-};
-
-
-/**
- * Just two dependencies to be able to render a viewport with panels in order
- * to make sure that the mode is working.
- */
 const extensionDependencies = {
   '@ohif/extension-default': '^3.0.0',
   '@ohif/extension-cornerstone': '^3.0.0',
-  '@ohif/extension-cornerstone-dicom-seg': '^3.0.0',
-  'ohif-extension-webquiz': '^0.0.1'
+  'ohif-extension-webquiz': '0/0/1',
 };
 
 function modeFactory({ modeConfiguration }) {
   return {
-    /**
-     * Mode ID, which should be unique among modes used by the viewer. This ID
-     * is used to identify the mode in the viewer's state.
-     */
     id,
-    routeName: 'routeWebQuiz',
-    /**
-     * Mode name, which is displayed in the viewer's UI in the workList, for the
-     * user to select the mode.
-     */
+    routeName: 'webquiz',
     displayName: 'WebQuiz Mode',
-    /**
-     * Runs when the Mode Route is mounted to the DOM. Usually used to initialize
-     * Services and other resources.
-     */
-    onModeEnter: ({ servicesManager, extensionManager, commandsManager }: withAppTypes) => {
-      const { measurementService, toolbarService, toolGroupService } = servicesManager.services;
+
+    onModeEnter:({servicesManager, extensionManager }: withAppTypes) => {
+      const { measurementService, toolbarService, toolGroupService } =
+        servicesManager.services;
 
       measurementService.clearMeasurements();
 
       // Init Default and SR ToolGroups
       initToolGroups(extensionManager, toolGroupService, commandsManager);
 
-      toolbarService.register([...toolbarButtons]);
-      toolbarService.updateSection('primary', [
-        'MeasurementTools',
-        'Zoom',
-        'Pan',
-        'TrackballRotate',
-        'WindowLevel',
-        'Capture',
-        'Layout',
-        'Crosshairs',
-        'MoreTools',
-      ]);
-
-      toolbarService.updateSection('MeasurementTools', [
-        'Length',
-        'Bidirectional',
-        'ArrowAnnotate',
-        'EllipticalROI',
-        'RectangleROI',
-        'CircleROI',
-        'PlanarFreehandROI',
-        'SplineROI',
-        'LivewireContour',
-      ]);
-
-      toolbarService.updateSection('MoreTools', [
-        'Reset',
-        'rotate-right',
-        'flipHorizontal',
-        'ImageSliceSync',
-        'ReferenceLines',
-        'ImageOverlayViewer',
-        'StackScroll',
-        'invert',
-        'Probe',
-        'Cine',
-        'Angle',
-        'CobbAngle',
-        'Magnify',
-        'CalibrationLine',
-        'TagBrowser',
-        'AdvancedMagnify',
-        'UltrasoundDirectionalTool',
-        'WindowLevelRegion',
-      ]);
+    
     },
+
     onModeExit: ({ servicesManager }: withAppTypes) => {
       const {
         toolGroupService,
@@ -125,7 +63,6 @@ function modeFactory({ modeConfiguration }) {
       segmentationService.destroy();
       cornerstoneViewportService.destroy();
     },
-    /** */
     validationTags: {
       study: [],
       series: [],
@@ -137,52 +74,32 @@ function modeFactory({ modeConfiguration }) {
     isValidMode: ({ modalities }) => {
       return { valid: true };
     },
-    /**
-     * Mode Routes are used to define the mode's behavior. A list of Mode Route
-     * that includes the mode's path and the layout to be used. The layout will
-     * include the components that are used in the layout. For instance, if the
-     * default layoutTemplate is used (id: '@ohif/extension-default.layoutTemplateModule.viewerLayout')
-     * it will include the leftPanels, rightPanels, and viewports. However, if
-     * you define another layoutTemplate that includes a Footer for instance,
-     * you should provide the Footer component here too. Note: We use Strings
-     * to reference the component's ID as they are registered in the internal
-     * ExtensionManager. The template for the string is:
-     * `${extensionId}.{moduleType}.${componentId}`.
-     */
     routes: [
       {
-        path: 'routeWebQuiz',
+        path: 'webquiz',
         layoutTemplate: ({ location, servicesManager }) => {
           return {
             id: ohif.layout,
             props: {
               leftPanels: [ohif.leftPanel],
               leftPanelResizable: true,
-              rightPanels: [ 'ohif-extension-webquiz.panelModule.webquiz', segmentation.panelTool, ohif.rightPanel],
+              rightPanels: [ 'ohif-extension-webquiz.panelModule.webquiz', cornerstone.measurements, cornerstone.segpanel],
               viewports: [
                 {
                   namespace: cornerstone.viewport,
                   displaySetsToDisplay: [ohif.sopClassHandler],
-                },
-                {
-                  namespace: segmentation.viewport,
-                  displaySetsToDisplay: [segmentation.sopClassHandler],
                 },
               ],
             },
           };
         },
       },
-    ],
-    /** List of extensions that are used by the mode */
+    ],  //routes
     extensions: extensionDependencies,
-    /** HangingProtocol used by the mode */
-    // hangingProtocol: [''],
-    /** SopClassHandlers used by the mode */
+    hangingProtocol: 'default',
     sopClassHandlers: [ohif.sopClassHandler],
-    /** hotkeys for mode */
-  };
-}
+  } //return
+};  //mode factory
 
 const mode = {
   id,
